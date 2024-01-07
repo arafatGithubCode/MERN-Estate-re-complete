ca9fc53a2cddc24f5a7896e5bcbb9ad7cc7f8169
arafatGithubCode <md489288@gmail.com>
2024-01-07 19:15:01 +0600

    create sign up UI and test api route

--- api/app.js
+++ api/app.js
@@ -1,5 +1,20 @@
 import express from "express";
+import userRouter from "./routes/user.route.js";
 
 const app = express();
+
+app.use(express.json());
+
+app.use("/api/user", userRouter);
+
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Server Error";
+  return res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
+});
 
 export default app;
--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,5 +1,44 @@
+import { Link } from "react-router-dom";
 const SignUp = () => {
-  return <section>SignUp</section>;
+  return (
+    <section className="p-3 max-w-lg mx-auto">
+      <h1 className="text-center py-7 text-3xl font-semibold text-slate-950">
+        Sign Up
+      </h1>
+      <form className="flex flex-col gap-4">
+        <input
+          className="p-3 rounded-lg border shadow"
+          type="text"
+          placeholder="Username"
+          id="username"
+        />
+        <input
+          className="p-3 rounded-lg border shadow"
+          type="email"
+          placeholder="Email"
+          id="email"
+        />
+        <input
+          className="p-3 rounded-lg border shadow"
+          type="password"
+          placeholder="Password"
+          id="password"
+        />
+        <button
+          className="p-3 rounded-lg border shadow bg-slate-700 text-white uppercase font-semibold"
+          type="submit"
+        >
+          sign up
+        </button>
+      </form>
+      <p>
+        Have an account?{" "}
+        <Link className="text-blue-700" to="/signin">
+          Sign in
+        </Link>
+      </p>
+    </section>
+  );
 };
 
 export default SignUp;
